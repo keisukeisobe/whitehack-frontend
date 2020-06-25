722a06abbdad0aff5437c673012cd8a698886276
Victor Ng <vng@brandeis.edu>
2020-06-24 20:02:16 -0500

    removed button duplication

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,12 +3,6 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <Link className="navbar-brand" to="https://www.reddit.com/r/Whitehack/">SW</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse"
-          data-target="#navbarCollapse" aria-controls="navbarCollapse"
-          aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
       <Link className="navbar-brand" to="https://www.reddit.com/r/Whitehack/">
         SW
       </Link>
@@ -23,6 +17,7 @@ function Header() {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
+
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
