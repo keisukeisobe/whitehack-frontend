ed615d5e6c0a63f874599db19adf707ffc26340d
Victor Ng <vng@brandeis.edu>
2020-06-24 19:50:21 -0500

    fixed router issue

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,9 +1,9 @@
-import React, {Component, ComponentType, useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component, ComponentType, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 function Header() {
-
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<<<<<<< Updated upstream
       <a className="navbar-brand" href="https://www.reddit.com/r/Whitehack/">SW</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse"
           data-target="#navbarCollapse" aria-controls="navbarCollapse"
@@ -21,6 +21,62 @@ function Header() {
         <li className="nav-item">
           <Link className="nav-link" to="/login" />
         </li>
+=======
+      <Link className="navbar-brand" to="https://www.reddit.com/r/Whitehack/">
+        SW
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/index">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/chargen">
+              Chargen
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/background">
+              Background
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav navbar-nav mt-2 mt-md-0">
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Profile
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Logout
+            </Link>
+          </li>
+>>>>>>> Stashed changes
         </ul>
       </div>
     </nav>
---
+++ src/Pages/chargen.tsx
@@ -0,0 +1,11 @@
+import React, { Component, ComponentType, useState, useEffect } from "react";
+
+function Chargen() {
+  return (
+    <div>
+      <h2>Chargen</h2>
+    </div>
+  );
+}
+
+export default Chargen;
