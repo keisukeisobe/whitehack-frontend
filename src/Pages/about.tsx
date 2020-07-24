308d1826eb278ebc7f6eb04fd1aa59c20fc71419
Victor Ng <vng@brandeis.edu>
2020-07-24 02:07:04 -0500

    Update about.tsx

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -1,6 +1,10 @@
 import React, { Component, ComponentType, useState, useEffect } from "react";
 
 function About() {
+  fetch("http://localhost:8000/api/equipment")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
     <div>
       <h2>About</h2>
